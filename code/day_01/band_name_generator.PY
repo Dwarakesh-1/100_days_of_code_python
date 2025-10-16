1ac613920bbfd7b14c1e26a77d9fa217fd074c5c
Dwarakesh Nallandula <127718128+Dwarakesh-1@users.noreply.github.com>
2025-10-16 13:05:36 -0500

    Add band name generator functionality

---
+++ code/day_01/band_name_generator.PY
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city_name = input("Enter the name of the city you grew up?\n")
+pet_name = input("Enter the name of your pet\n")
+print(f"The band name will be {city_name} {pet_name}")
